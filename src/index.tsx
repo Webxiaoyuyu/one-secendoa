12842f15d400a54821381365ad3c3f0455297795
ZhengYu <11966314+wbexiaoyu@user.noreply.gitee.com>
2023-06-04 03:29:44 +0000

    !1 feat(other): 完成主题设置 (I7AP0V) * feat(other): 完成主题设置 (I7AP0V)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StyleProvider } from '@ant-design/cssinjs';
+import { ConfigProvider } from 'antd';
 import '@/assets/index.css';
 import App from '@/App';
 
@@ -8,7 +9,17 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <StyleProvider hashPriority="high">
-      <App />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#955ce6',
+            colorInfo: '#955ce6',
+            wireframe: false,
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
     </StyleProvider>
   </React.StrictMode>,
 );
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,34 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-// import { getSongListType, getTopPlaylist } from '@/service';
-// import { useRequest } from 'ahooks';
+import styled from 'styled-components';
+
+export const HomeCss = styled.div`
+  div {
+    background: #00ff2a;
+  }
+`;
 
 export default function Home() {
-  // const [songListType, setSongListType] = useState<ISongListTypeResponse['sub']>([]);
-  // const [songListType, setSongListType] = useState<
-  //   Awaited<ReturnType<typeof getSongListType>>['data']['sub']
-  // >([]);
-  // useEffect(() => {
-  //   getSongListType()
-  //     .then((res) => {
-  //       setSongListType(res.data.sub);
-  //     })
-  //     .catch(() => {});
-  // }, []);
-  // const { data: topPlaylist } = useRequest(() => getTopPlaylist({ cat: '全部' }));
   return (
-    <>
-      <div className=" w-[500px] h-[500px] bg-red-500">test</div>
-      {/* <ul>
-        {topPlaylist?.map((item) => (
-          <li key={item.id}>{item.description}</li>
-        ))}
-      </ul>
-      <ul>
-        {songListType.map((item) => (
-          <li key={item.name}>{item.name}</li>
-        ))}
-      </ul> */}
-    </>
+    <HomeCss>
+      <div className=" w-[500px] h-[500px]">test</div>
+    </HomeCss>
   );
 }
