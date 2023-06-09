8a2ab0768884714c278f29ebefb3ecbc57865ba1
Zhumeng99 <1410157844@qq.com>
2023-06-09 10:05:36 +0800

    feat(views): 完成代理管理页面
    closes #I7AR4L

--- src/service/index.ts
+++ src/service/index.ts
@@ -67,3 +67,118 @@ export const getAdminSuper = () =>
 // 修改密码
 export const editAdminUpdatepwd = (data: Iupdatepwd) =>
   request.put('/admin/updatepwd', data) as AxiosPromise<TUpdatepwdResponseData>;
+
+// 代理列表
+interface GetAgentListParams {
+  current: number;
+  pageSize: number;
+  agentAccount?: string;
+  agentNo?: string;
+  mobileNumber?: string;
+  realName?: string;
+  status?: number;
+}
+
+type IsAgentList = {
+  code: number;
+  msg: string;
+  data: {
+    pageSize: number;
+    current: number;
+    count: number;
+    totalPages: number;
+    data: {
+      agentNo: string;
+      agentAccount: string;
+      mobileNumber: string;
+      realName: string;
+      status: number;
+      createTime: string;
+      updateTime: string;
+      defaultPwd: string;
+      updatedBy: string;
+    }[];
+  };
+};
+
+export const getAgentList = async (params: GetAgentListParams) => {
+  const res = request.get('/admin/agent/list', { params }) as AxiosPromise<IsAgentList>;
+  return (await res).data.data;
+};
+
+// 改变状态
+interface GetStatusParams {
+  agentNo: string;
+  status: number;
+}
+export const getStatus = async (params: GetStatusParams) => {
+  const res = request.put('/admin/agent/status', params);
+  return (await res).data;
+};
+
+// 添加代理
+interface GetAdd {
+  agentAccount: string;
+  realName: string;
+  mobileNumber: string;
+  status: number;
+}
+export const getAdd = async (params: GetAdd) => {
+  const res = request.post('/admin/agent/add', params);
+  return (await res).data;
+};
+
+// 重置密码
+interface GetPwd {
+  agentNo: string;
+}
+export const getChangePwd = async (params: GetPwd) => {
+  const res = request.put('/admin/agent/resetpwd', params);
+  return (await res).data;
+};
+
+// 点击跳转到详情页面
+interface GetList {
+  adminNo?: string | null;
+  pageSize: number;
+  current: number;
+  agentAccount?: string;
+  agentNo?: string;
+  mobileNumber?: string;
+  realName?: string;
+  status?: number;
+}
+export const getList = async (params: GetList) => {
+  const res = request.get('/admin/list', { params });
+  return (await res).data;
+};
+
+// 详情页面的切换状态
+interface GetAdminStatusParams {
+  adminNo: string | null;
+  status: number;
+}
+export const getAdminStatus = async (params: GetAdminStatusParams) => {
+  const res = request.put('/admin/status', params);
+  return (await res).data;
+};
+
+// 详情页面重置密码
+interface GetAdminPwd {
+  adminNo: string | null;
+}
+export const getChangeAdminPwd = async (params: GetAdminPwd) => {
+  const res = request.put('/admin/resetpwd', params);
+  return (await res).data;
+};
+
+// 添加管理员
+interface GetAdminAdd {
+  adminName: string;
+  realName: string;
+  mobileNumber: string;
+}
+export const getAdminAdd = async (params: GetAdminAdd) => {
+  const res = request.post('/admin/add', params);
+  return (await res).data;
+};
---
+++ src/views/user/agent/Add.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Form, Input, Radio, Button, message } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+import { getAdd } from '@/service/index';
+import { useRequest } from 'ahooks';
+import { useNavigate } from 'react-router-dom';
+
+export const AgentCss = styled.div`
+  div {
+    .ant-input {
+      height: 40px !important;
+    }
+    .ant-btn {
+      height: 40px;
+      padding: 0 15px;
+      font-size: 14px;
+      border-radius: 4px;
+    }
+  }
+`;
+const Add: React.FC = () => {
+  const [form] = Form.useForm();
+  const [messageApi, contextHolder] = message.useMessage();
+  const { run: runAdd } = useRequest(getAdd, {
+    manual: true,
+    onSuccess(res) {
+      messageApi.open({
+        type: 'success',
+        content: `${res.msg}`,
+      });
+    },
+  });
+  const onFinish = (values: any) => {
+    if (values.radioGroup === undefined) {
+      values.radioGroup = 1;
+    }
+    runAdd({
+      agentAccount: `${values.name}`,
+      realName: `${values.age}`,
+      mobileNumber: `${values.phone}`,
+      status: values.radioGroup,
+    });
+  };
+  const navigate = useNavigate();
+  return (
+    <AgentCss className="p-[20px]">
+      {contextHolder}
+      <div className="flex px-[24px] py-[16px]">
+        <div className="mx-[8px] mr-[16px] text-[16px]" onClick={() => navigate(-1)}>
+          <ArrowLeftOutlined />
+        </div>
+        <div className="pr-[12px] text-[#333333] font-semibold text-[20px] leadding-[32px]">
+          新增代理
+        </div>
+      </div>
+      {/* 表单部分 */}
+      <div className="w-[600px] px-[50px]">
+        {/* 表单上 */}
+        <Form
+          form={form}
+          name="validateOnly"
+          layout="vertical"
+          autoComplete="off"
+          onFinish={onFinish}
+        >
+          <Form.Item name="name" label="账户名称" rules={[{ required: true }]}>
+            <Input placeholder="请输入账户名称" />
+          </Form.Item>
+          <Form.Item name="age" label="真实名称" rules={[{ required: true }]}>
+            <Input placeholder="请输入真实姓名" />
+          </Form.Item>
+          <Form.Item name="phone" label="手机号" rules={[{ required: true }]}>
+            <Input placeholder="请输入手机号" />
+          </Form.Item>
+          {/* 单选框 */}
+          <Form.Item name="radioGroup" label="是否启用">
+            <Radio.Group defaultValue={1}>
+              <Radio value={1}>启用</Radio>
+              <Radio value={0}>禁用</Radio>
+            </Radio.Group>
+          </Form.Item>
+          {/* 按钮 */}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              提交保存
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </AgentCss>
+  );
+};
+
+export default Add;
